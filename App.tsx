ac09cfeb9bbff0d160eb59a24f166c90eeef4ca0
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-07-18 23:47:41 -0300

    expo fonts google

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { NativeBaseProvider } from "native-base"
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { SignIn } from './src/screens/SingIn';
 import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn/>
+      { fontsLoaded ? <SignIn/> : <Loading />}
     </NativeBaseProvider>
   );
 }
