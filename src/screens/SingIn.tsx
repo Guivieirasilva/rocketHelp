b10bd47cde90d7190ab33e62a5f9cbe819b8d3bb
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-07-22 14:39:17 -0300

    dependences firebase

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,5 +1,6 @@
 import {useState} from "react"
-
+import { Alert } from "react-native"
+import auth from "@react-native-firebase/auth"
 import { VStack, Heading, Icon, useTheme } from "native-base"
 import Logo from "../assets/logo_primary.svg"
 import { Envelope, Key} from "phosphor-react-native"
@@ -10,24 +11,34 @@ import { Button } from "../components/Button"
 
 export function SignIn() {
 
-    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const {colors} = useTheme()
 
+    function handleSingIn(){
+        if(!email || !password){
+           return Alert.alert('Entrar', "Informe e-mail e senha")
+        }
+    }
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={20}>
+
             <Logo />
+
             <Heading color="gray.100" fontSize="xl" mt={20} mb={4}>
                 Acesse sua conta 
             </Heading>
+
             <Input 
             placeholder="E-mail"
             mb={4}
             InputLeftElement={
             <Icon as={<Envelope color={colors.gray[300] } />} 
             ml={6}/>}
-            onChangeText={setName}
+            onChangeText={setEmail}
+            
             />
 
             <Input 
@@ -42,6 +53,7 @@ export function SignIn() {
             <Button 
             title="Entrar"
             w="full"
+            onPress={handleSingIn}
             />
             
         </VStack>        
